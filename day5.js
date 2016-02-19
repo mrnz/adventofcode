949f23219e52210cd785962c46380411bca45b60
Przemek <mrnz@wp.pl>
2016-02-19 12:21:02 +0000

    day 5 part 2

--- day5.js
+++ day5.js
@@ -42,3 +42,45 @@ var timer = require("./helpers/timer.js");
 	console.log("Result is: " + result + ' In ' + timer.stop() + ' sec' );
 
 })(data);
+
+
+
+/*--- Part Two ---
+
+Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. None of the old rules apply, as they are all clearly ridiculous.
+
+Now, a nice string is one with all of the following properties:
+
+It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+For example:
+
+qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+How many strings are nice under these new rules?
+*/
+
+(function(input) {
+	
+	var result, test1, test2, test3, startTime;
+
+	timer.start();
+
+	result = 0;	
+	test1 = /\w*(\w{2})\w*\1/;
+	test2 = /(\w)\w\1/;
+	
+	
+	input.forEach(function(string, index) {
+		
+		if( test1.test(string) && test2.test(string) ){
+			result++;
+		}
+			
+	});
+	
+	console.log("Result is: " + result + ' In ' + timer.stop() + ' sec' );
+
+})(data);
