8276862094dd63c748a4937a59847c38f6515230
Przemek <mrnz@wp.pl>
2016-03-02 11:25:41 +0000

    day 14 - update

--- day14.js
+++ day14.js
@@ -97,29 +97,29 @@ how many points does the winning reindeer have?
 
 (function (data, totalTime) {
   
-  var results = [];
+  // initialize result array with length equals reindeers number and starting points 0
+  var results = Array.apply(null, Array(data.length)).map(function(){return 0;});
   
-  for (var i = 1; i <= totalTime; i++) {  
+  for (var time = 1; time <= totalTime; time++) {  
 
-    var distancesInTimePoint = [];
-
+    // initialize temp array with length equals reindeers number and starting distance 0
+    var distancesInTimePoint = Array.apply(null, Array(data.length)).map(function(){return 0;});
 
     data.forEach(function(line, index) {
       
-      var time = i,
-          distance = 0,
+      var distance = 0,
           phase = true,
           memo = time,
           speed = parseInt(line.match(/\d+ km/g)),
           flyTime = parseInt(line.match(/\d+ se/g)[0]),
           restTime = parseInt(line.match(/\d+ se/g)[1]);
 
-      results[index] = results[index] || 0;
-      distancesInTimePoint[index] = distancesInTimePoint[index] || 0;
-
       (function calculatePosition () {
+
         if(memo === 0)return;
+
         if(phase){
+          // fly phase
           if(memo > flyTime){
             distance+=speed*flyTime;
             memo-=flyTime;
@@ -127,12 +127,15 @@ how many points does the winning reindeer have?
             distance+=speed*memo;
             memo-=memo;
           }
+
         }else{
+          // rest phase
           if(memo > restTime){
             memo-=restTime;
           }else{
             memo-=memo;
           }
+
         }
         phase = !phase;
         calculatePosition();
@@ -143,17 +146,19 @@ how many points does the winning reindeer have?
 
     });
     
-        
+    // find indexes of the highest values at this time point
     resultsAtThisTimePoint = distancesInTimePoint.reduce(function(previousValue, currentValue, currentIndex) {
       return previousValue.concat(currentValue === Math.max.apply(Math, distancesInTimePoint) ? [currentIndex] : []);
     },[]);
     
+    // distribute points to according to indexes
     resultsAtThisTimePoint.forEach(function(v,i) {
       results[v] += 1;
     });
     
   };
 
+  // display the highest result 
   console.log(Math.max.apply(Math, results ) );
 
 })(lines, 2503);
