c01176d27cf4af98c6ca20ccfe45018351ee51e9
Przemek <mrnz@wp.pl>
2016-03-29 15:55:48 +0100

    restructure

--- day08/8-1.js
+++ day08/8-1.js
@@ -4,22 +4,23 @@ module.exports = function(data) {
 	var string = 0;
 
 	data.forEach(function(line) {
-	    
-	    total += line.length;
-	    
-	    // get rid of opening and cloasing quotes
-	    line = line.slice(1, line.length-1);
-	    
-	    var regex = new RegExp('\\\\"', 'g');
-	    line = line.replace(regex, '1');
-	    
-	    var regex = new RegExp('\\\\\\\\', 'g');
-	    line = line.replace(regex, '1');
-	    
-	    var regex = new RegExp('\\\\x([a-f0-9]{2})', 'g');
-	    line = line.replace(regex, '1');
+		
+		total += line.length;
+		
+		// get rid of opening and cloasing quotes
+		line = line.slice(1, line.length-1);
+		
+		var regex = new RegExp('\\\\"', 'g');
+		line = line.replace(regex, '1');
+		
+		var regex = new RegExp('\\\\\\\\', 'g');
+		line = line.replace(regex, '1');
+		
+		var regex = new RegExp('\\\\x([a-f0-9]{2})', 'g');
+		line = line.replace(regex, '1');
 
-	    string += line.length;
+		string += line.length;
+		
 	});
 
 	return total - string;
