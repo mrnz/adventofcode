845896523d53aba275213dc66a4536737ba0862c
Przemek <mrnz@wp.pl>
2016-03-29 15:59:29 +0100

    restructure

--- day07/7-1.js
+++ day07/7-1.js
@@ -1,27 +1,27 @@
 module.exports = function(data, toFind, overwriteChar, overwriteVal) { 
 
-	var all = [], init, find, cache = [];
+  var all = [], init, find, cache = [];
 
-	init = (item) => {
-		var temp = item.split(" -> "),
-			target = temp[1],
-			expresion = temp[0];
-		
-		cache[target] = NaN;
-		all[target] = isNaN(expresion) ? expresion : parseInt(expresion);
-	};
+  init = (item) => {
+    var temp = item.split(" -> "),
+      target = temp[1],
+      expresion = temp[0];
+    
+    cache[target] = NaN;
+    all[target] = isNaN(expresion) ? expresion : parseInt(expresion);
+  };
 
-	find = (toFind) => {
+  find = (toFind) => {
 
-		if(!isNaN(toFind)) return toFind;
+    if(!isNaN(toFind)) return toFind;
     if(!isNaN(cache[toFind])) return cache[toFind];
     if(!isNaN(all[toFind])) { 
         cache[toFind] = all[toFind];
         return cache[toFind];
-    }		
-		var args = all[toFind].split(" ");
-		
-		if(args.length == 1) {
+    }   
+    var args = all[toFind].split(" ");
+    
+    if(args.length == 1) {
       cache[toFind] = find(args[0]);
       return cache[toFind];
     };
@@ -32,12 +32,12 @@ module.exports = function(data, toFind, overwriteChar, overwriteVal) {
       cache[toFind] = not;
       return cache[toFind];
     }
-		if(args.length == 3) {
+    if(args.length == 3) {
       if(args[1] == "AND") {
         var res = find(args[0], all, cache) & find(args[2], all, cache);
         cache[toFind] = res;
         return cache[toFind];
-	    }
+      }
       if(args[1] == "OR") {
         var res = find(args[0], all, cache) | find(args[2], all, cache);
         cache[toFind] = res;
@@ -55,15 +55,15 @@ module.exports = function(data, toFind, overwriteChar, overwriteVal) {
       }
     }
 
-	};
+  };
 
-	data.forEach( line => {
-		init(line);
-	});
-	if( typeof overwriteChar !== 'undefined' ){
-		all[overwriteChar] = overwriteVal;	
-	}
-	
-	return find("a");
-	
+  data.forEach( line => {
+    init(line);
+  });
+  if( typeof overwriteChar !== 'undefined' ){
+    all[overwriteChar] = overwriteVal;  
+  }
+  
+  return find("a");
+  
 };
