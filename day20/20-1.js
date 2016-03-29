019c27868465322a73d664d5930355f7979e237c
Przemek <mrnz@wp.pl>
2016-03-29 09:54:29 +0100

    day 20 - format

--- day20/20-1.js
+++ day20/20-1.js
@@ -1,40 +1,41 @@
 module.exports = function(input, repetitionNumber, limit) {
-	
-	var presents = [];
+  
+  var presents = [];
 
-	for (var  e = 1; e < input / repetitionNumber; e++) {
-	  
-	  var visits = 0;
+  for (var  e = 1; e < input / repetitionNumber; e++) {
+    
+    var visits = 0;
 
-	  for (var i = e; i < input / repetitionNumber; i = i + e) {
-	    
-	  	if(limit){
-				
-				if (visits < limit) {
-		      
-		      if (!presents[i]){
-			      presents[i] = (repetitionNumber+1);
-			    }
+    for (var i = e; i < input / repetitionNumber; i = i + e) {
+      
+      if(limit){
+        
+        if (visits < limit) {
+          
+          if (!presents[i]){
+            presents[i] = (repetitionNumber+1);
+          }
 
-			    presents[i] = presents[i] + e * (repetitionNumber+1);
-			    visits = visits + 1;
+          presents[i] = presents[i] + e * (repetitionNumber+1);
+          visits = visits + 1;
 
-	    	};
+        };
 
-	  	}else{
+      }else{
 
-	  		if (!presents[i]){
-	     		presents[i] = repetitionNumber;
-	    	};
+        if (!presents[i]){
+          presents[i] = repetitionNumber;
+        };
 
-	    	presents[i] = presents[i] + e * repetitionNumber;
+        presents[i] = presents[i] + e * repetitionNumber;
 
-	  	}
+      }
 
-	  }
-	}
+    }
+  }
 
-	return presents.reduce(function(min, current, index){
-		return (min === 0 && current >= input) ? min = index : min;
-	},0);
+  return presents.reduce(function(min, current, index){
+    return (min === 0 && current >= input) ? min = index : min;
+  },0);
+  
 };
