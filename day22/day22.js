6af16de443bde8f3e493268c2ad228e2b03b8bfc
Przemek <mrnz@wp.pl>
2016-03-29 14:54:11 +0100

    day 22 skeleton

---
+++ day22/day22.js
@@ -0,0 +1,8 @@
+var d22_1 = require('./22-1.js');
+
+// Hit Points: 51
+// Damage: 9
+
+
+console.log( 'day 22 part 1 - result is: ' + d22_1() );
+// console.log( 'day 21 part 2 - result is: ' + d21_1(input, false) );
