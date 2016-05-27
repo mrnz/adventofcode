a618685c88869fb516d3f4ac45ae723e3736984e
Przemek <mrnz@wp.pl>
2016-05-27 23:53:56 +0100

    coverage fix 6

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# adventfocode
+# Advent of Code
 
 [![Travis](https://img.shields.io/travis/mrnz/adventofcode.svg?maxAge=20&style=plastic)](https://travis-ci.org/mrnz/adventofcode) [![Codecov](https://img.shields.io/codecov/c/github/mrnz/adventofcode.svg?maxAge=20&style=plastic)](https://codecov.io/gh/mrnz/adventofcode)
 
-my solutions of problems from adventofcode.com
+my solutions of problems from [adventofcode.com](http://adventofcode.com)
--- test/test-day06.js
+++ test/test-day06.js
@@ -7,19 +7,23 @@ describe('Day 6', function() {
   describe('Part One', function() {
 
     it('should return 1000000 when input is "turn on 0,0 through 999,999', function () {
-      this.timeout(5000);
+      this.timeout(10000);
       assert.equal( 1000000, day6part1(['turn on 0,0 through 999,999']) );
 
     });
 
     it('should return 1000 when input is "toggle 0,0 through 999,0"', function () {
-      this.timeout(5000);
+      this.timeout(10000);
       assert.equal( 1000, day6part1(['toggle 0,0 through 999,0']) );
     });
 
     it('should return 0 when input is "turn on 499,499 through 500,500"', function () {
-      this.timeout(5000);
+      this.timeout(10000);
       assert.equal( 1, day6part1(['turn on 499,499 through 499,499']) );
+    });
+        it('should return 1 when input is "toggle on 0,0 through 0,0", "turn off on 0,0 through 0,0"', function () {
+      this.timeout(10000);
+      assert.equal( 0, day6part1(['toggle on 0,0 through 0,0', 'turn off on 0,0 through 0,0']) );
     });
 
   });
@@ -27,13 +31,17 @@ describe('Day 6', function() {
   describe('Part Two', function() {
 
     it('should return 242 when input is "toggle 0,0 through 10,10"', function () {
-      this.timeout(5000);
+      this.timeout(10000);
       assert.equal( 242, day6part2(['toggle 0,0 through 10,10']) );
     });
 
     it('should return 1 when input is "turn on 0,0 through 0,0"', function () {
-      this.timeout(5000);
+      this.timeout(10000);
       assert.equal( 1, day6part2(['turn on 0,0 through 0,0']) );
+    });
+    it('should return 1 when input is "toggle on 0,0 through 0,0", "turn off on 0,0 through 0,0"', function () {
+      this.timeout(10000);
+      assert.equal( 1, day6part2(['toggle on 0,0 through 0,0', 'turn off on 0,0 through 0,0']) );
     });
 
   });
