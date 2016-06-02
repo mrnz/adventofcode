eb651f46194b01fac6e848d923261e1280fc70ae
Przemek <mrnz@wp.pl>
2016-06-02 21:14:12 +0100

    day 23 part 1 + tests

---
+++ day23/23-1.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = function(input, register) {
+	var idx = 0,
+			r = {
+				a:0,
+				b:0
+			},
+			register = typeof register === 'undefined' ? 'b' : register;
+	
+	while(idx >= 0 && input.length>idx){
+		
+		let temp = input[idx].split(' ');
+		let command = temp[0];
+
+		switch(command) {
+	    case 'hlf':
+	      r[temp[1]] /= 2;
+	      idx += 1;
+	      break;
+	    case 'tpl':
+	      r[temp[1]] *= 3;
+	      idx += 1;
+	      break;
+	    case 'inc':
+	      r[temp[1]] += 1;
+	      idx += 1;
+	      break;
+	    case 'jmp':
+	      idx += parseInt( temp[1] );
+	      break;
+	    case 'jie':
+	    	idx += r[temp[1][0]]%2===0 ? parseInt( temp[2] ) : 1;
+	      break;
+	    case 'jio':
+				idx += r[temp[1][0]]===1 ? parseInt( temp[2] ) : 1;
+	      break;                
+		}
+	};
+	return r[register];
+}
---
+++ test/test-day23.js
@@ -0,0 +1,29 @@
+var assert = require('assert'),
+		fs = require('fs'),
+    input = fs.readFileSync(__dirname+'/../day23/day23-input.txt').toString().split('\n'),
+    d23_1 = require('./../day23/23-1.js');
+
+describe('Day 23', function() {
+
+  describe('Part One', function() {
+    var test1 = ['inc a','jio a, +2','tpl a','inc a'];
+    it('should return 2 when input is a example from adventofcode.com', function () {
+      assert.equal( 2, d23_1(test1,'a') );
+    }); 
+
+    var test2 = ['jmp +1','inc a','jio a, +1','inc a','jie a, +1','tpl a'];
+    it('should return 6 when input is a my example', function () {
+      assert.equal( 6, d23_1(test2,'a') );
+    }); 
+
+  });
+
+  // describe('Part Two', function() {
+    
+  //   it('should return 188 when input is from adventofcode.com and win is false', function () {
+  //     assert.equal( 188, d23_1(input,false) );
+  //   }); 
+     
+  // });
+
+});
