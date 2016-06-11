16d4317fc712d675a3d97cb3157255ed7c1814b7
Przemek <mrnz@wp.pl>
2016-06-11 14:52:41 +0100

    refactoring day4

--- day04/4-1.js
+++ day04/4-1.js
@@ -1,23 +1,17 @@
+'use strict';
 var md5 = require("./../node_modules/md5/md5.js");
 
 module.exports = function(firstZeros, prefix) {
 
-  var result, 
-      n, 
-      patern = '0000000000000'.slice(0,firstZeros); 
-
-  result = false;
-  n = 0;
+  var result = false, 
+      patern = '0'.repeat(firstZeros);
     
-  while(result === false){
+  for( let n = 0; !result; n++){
+    
+    let hash = md5( prefix + n ).toString().slice(0,firstZeros);
 
-    var hash = md5( prefix + n.toString() ).toString();
+    if( hash === patern)result = n;
 
-    if(hash.slice(0,firstZeros) === patern){
-      result = n;
-    };
-
-    n++;
   };
   
   return result;
--- day04/day4.js
+++ day04/day4.js
@@ -1,6 +1,3 @@
-var prefix = 'yzbqklnj',
-    d4 = require('./4-1.js');
-
-console.log( 'day 4 part 1 - result is: ' + d4(5, prefix) );
-console.log( 'day 4 part 2 - result is: ' + d4(6, prefix) );
+console.log( `day 4 part 1 - result is: ${require('./4-1.js')(5, 'yzbqklnj')}` );
+console.log( `day 4 part 2 - result is: ${require('./4-1.js')(6, 'yzbqklnj')}` );
 
