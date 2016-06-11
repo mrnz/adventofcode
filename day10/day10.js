efa64fb5e6144a768ef278fe021f637726820fa7
Przemek <mrnz@wp.pl>
2016-06-11 23:34:11 +0100

    refactoring day10

--- day10/day10.js
+++ day10/day10.js
@@ -1,4 +1,2 @@
-var d10 = require('./10-1.js');
-
-console.log( 'day 10 part 1 - result is: ' + d10("3113322113", 40) );
-console.log( 'day 10 part 2 - result is: ' + d10("3113322113", 50) );
+console.log( `day 10 part 1 - result is: ${require('./10-1.js')("3113322113", 40)}` );
+console.log( `day 10 part 2 - result is: ${require('./10-1.js')("3113322113", 50)}` );
